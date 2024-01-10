3a855293c0651c80156c415d283f15330dab044b
KhalidMgithubio <145397882+KhalidMgithubio@users.noreply.github.com>
2024-01-10 15:16:35 -0600

    New loading screen
    made new loading screen
    i have a little problem with a button or block of white on the brown screen

--- libraries/loading.js
+++ libraries/loading.js
@@ -1,99 +1,203 @@
-// let isStarted = false;//action
-// let startButton;
-// let backButton;
-// let colorButtons = [];
-// let actionButtons = [];
-// let Example;
-// function preload(){
-//   Example = loadImage("assets/Example.png"); 
-// }
+// let startButton, backButton, playButton, back2Button;
+// let isStartScreen = true;
+// let isPlayScreen = false;
+// let squaresVisible = false;
+// let redButtons, blueButtons;
+// let redSquareColor, blueSquareColor;
+// let colorSequence = ["red", "blue", "yellow", "black"];
+// let currentColorIndexRed, currentColorIndexBlue;
+
+
 // function setup() {
 //   createCanvas(windowWidth, windowHeight);
 
-//   // Create a start button
-//   startButton = createImg("assets/Example.png");
+
+//   // Create the start button
+//   startButton = createButton('Start');
 //   startButton.position(width / 2 - 50, height / 2);
 //   startButton.mousePressed(startGame);
 
-  
 
-//   // Create a back button (initially hidden)
+//   // Create the back button but hide it initially
 //   backButton = createButton('Back');
-//   backButton.position(width - 120, 20);
-//   backButton.mousePressed(goBack);
+//   backButton.position(width - 80, 20);
 //   backButton.hide();
+//   backButton.mousePressed(goToStartScreen);
 
-//   // Create four color buttons with different colors and hide them initially
-//   for (let i = 0; i < 4; i++) {
-//     colorButtons[i] = createButton('Block ' + (i + 1));
-//     colorButtons[i].position(width / 4 * i + 50, height / 2 + 50);
-//     colorButtons[i].mousePressed(() => changeColor(i));
-//     colorButtons[i].hide();
-//   }
 
-//   // Create three action buttons (initially hidden)
-//   for (let i = 0; i < 3; i++) {
-//     actionButtons[i] = createButton('Action ' + (i + 1));
-//     actionButtons[i].position(width / 4 * i + 50, height / 2 + 150);
-//     actionButtons[i].hide();
-//   }
+//   // Create the play button but hide it initially
+//   playButton = createButton('Play');
+//   playButton.position(width / 2 - 50, height - 50);
+//   playButton.mousePressed(playGame);
+//   playButton.hide();
+
+
+//   // Create the back2 button but hide it initially
+//   back2Button = createButton('Back 2');
+//   back2Button.position(width - 80, 20);
+//   back2Button.hide();
+//   back2Button.mousePressed(goToBlackScreen);
+
+
+//   // Create arrays for red and blue buttons
+//   redButtons = createButtons(width / 2 - 300, height / 2, 200, 30, "red");
+//   blueButtons = createButtons(width / 2 + 100, height / 2, 200, 30, "blue");
+
+
+//   // Initialize square colors and color indices
+//   redSquareColor = "red";
+//   blueSquareColor = "blue";
+//   currentColorIndexRed = colorSequence.indexOf(redSquareColor);
+//   currentColorIndexBlue = colorSequence.indexOf(blueSquareColor);
 // }
+
 
 // function draw() {
-//   if (isStarted) {
-//     // Add other game logic or drawing here
+//   if (isStartScreen) {
+//     background(0, 0, 255); // Blue background for start screen
+//   } else if (isPlayScreen) {
+//     drawBrownScreen();
+//     back2Button.show(); // Show Back 2 button
 //   } else {
-//     background(220);
-//     // Add any initial screen drawing here
+//     background(0); // Black background after pressing start button
+//     if (squaresVisible) {
+//       drawSquare(width / 2 - 200, height / 2, redSquareColor); // Red square
+//       drawSquare(width / 2 + 200, height / 2, blueSquareColor); // Blue square
+//       playButton.show(); // Show play button
+//     }
 //   }
 // }
+
+
+// function drawSquare(x, y, squareColor) {
+//   fill(squareColor);
+//   noStroke();
+//   rectMode(CENTER);
+//   rect(x, y, 200, 200);
+// }
+
+
+// function drawBrownScreen() {
+//   background(139, 69, 19); // Brown background for play screen
+
+
+//   // Draw lines to form a perfect square grid
+//   stroke(0);
+//   strokeWeight(2);
+//   for (let y = 0; y < height; y += 45) {
+//     line(45, y, width-45, y);
+//   }
+//   for (let x = 0; x < width; x += 45) {
+//     line(x, 45, x, height-12);
+//   }
+
+
+// }
+
+
+// function createButtons(x, y, width, height, buttonColor) {
+//   let buttons = [];
+//   buttons.push(createButton('Up').position(x, y - height - 100).size(width, height).hide().mousePressed(() => changeColor("up", buttonColor)));
+//   buttons.push(createButton('Down').position(x, y + 100).size(width, height).hide().mousePressed(() => changeColor("down", buttonColor)));
+//   return buttons;
+// }
+
+
+// function changeColor(direction, buttonColor) {
+//   let currentColorIndex, otherColorIndex;
+
+
+//   if (buttonColor === "red") {
+//     currentColorIndex = currentColorIndexRed;
+//     otherColorIndex = currentColorIndexBlue;
+//   } else {
+//     currentColorIndex = currentColorIndexBlue;
+//     otherColorIndex = currentColorIndexRed;
+//   }
+
+
+//   if (direction === "up") {
+//     currentColorIndex = (currentColorIndex + 1) % colorSequence.length;
+//     while (currentColorIndex === otherColorIndex) {
+//       currentColorIndex = (currentColorIndex + 1) % colorSequence.length;
+//     }
+//   } else if (direction === "down") {
+//     currentColorIndex = (currentColorIndex + 3) % colorSequence.length;
+//     while (currentColorIndex === otherColorIndex) {
+//       currentColorIndex = (currentColorIndex + 3) % colorSequence.length;
+//     }
+//   }
+
+
+//   if (buttonColor === "red") {
+//     redSquareColor = colorSequence[currentColorIndex];
+//     currentColorIndexRed = currentColorIndex;
+//   } else {
+//     blueSquareColor = colorSequence[currentColorIndex];
+//     currentColorIndexBlue = currentColorIndex;
+//   }
+// }
+
 
 // function startGame() {
-//   isStarted = true;
-
-//   // Remove the start button after it is pressed
+//   isStartScreen = false;
+//   isPlayScreen = false;
 //   startButton.hide();
+//   backButton.show();
+//   squaresVisible = true;
 
-//   // Show the color buttons
-//   for (let i = 0; i < colorButtons.length; i++) {
-//     colorButtons[i].show();
+
+//   // Show buttons when starting the game
+//   for (let button of redButtons.concat(blueButtons)) {
+//     button.show();
 //   }
 // }
 
-// function changeColor(index) {
-//   if (isStarted) {
-//     // Change background color based on button index
-//     if (index === 0) {
-//       background(255, 0, 0); // Red (change to knight like calse thing)
-//     } else if (index === 1) {
-//       background(0, 0, 255); // Blue (forest)
-//     } else if (index === 2) {
-//       background(255, 255, 0); // Yellow (ork like camp)
-//     } else if (index === 3) {
-//       background(169); // Gray (nice)(mines)
-//     }
 
-//     // Hide the color buttons and show the action buttons after one is pressed
-//     for (let i = 0; i < colorButtons.length; i++) {
-//       colorButtons[i].hide();
-//     }
-//     for (let i = 0; i < actionButtons.length; i++) {
-//       actionButtons[i].show();
-//     }
-//     backButton.show();
-//   }
-// }
-
-// function goBack() {
-//   isStarted = false;
-
-//   // Show the start button and hide the color buttons, action buttons, and back button
+// function goToStartScreen() {
+//   isStartScreen = true;
+//   isPlayScreen = false;
 //   startButton.show();
-//   for (let i = 0; i < colorButtons.length; i++) {
-//     colorButtons[i].hide();
-//   }
-//   for (let i = 0; i < actionButtons.length; i++) {
-//     actionButtons[i].hide();
-//   }
 //   backButton.hide();
+//   back2Button.hide();
+//   squaresVisible = false;
+
+
+//   // Hide buttons and play button when going back to the start screen
+//   for (let button of redButtons.concat(blueButtons)) {
+//     button.hide();
+//   }
+//   playButton.hide();
+// }
+
+
+// function playGame() {
+//   isStartScreen = false;
+//   isPlayScreen = true;
+//   backButton.show();
+//   back2Button.hide();
+//   squaresVisible = false;
+
+
+//   // Hide buttons and play button when going to play screen
+//   for (let button of redButtons.concat(blueButtons)) {
+//     button.hide();
+//   }
+//   playButton.hide();
+// }
+
+
+// function goToBlackScreen() {
+//   isStartScreen = false;
+//   isPlayScreen = false;
+//   backButton.show();
+//   back2Button.hide();
+//   squaresVisible = true;
+
+
+//   // Show buttons when going back to the black screen
+//   for (let button of redButtons.concat(blueButtons)) {
+//     button.show();
+//   }
+//   playButton.show();
 // }
--- sketch.js
+++ sketch.js
@@ -4,67 +4,67 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-// let Battlemap2;
-// let unit;
-// let check = false;
-// let check2 = false;
-// let unit2;
-// let units = [];
-// let example;
-// function preload(){
-//   Battlemap2 = loadImage("Assests/Battlemap2.jpg");
-//   example = loadImage("Assests/Cav.png");
+let Battlemap2;
+let unit;
+let check = false;
+let check2 = false;
+let unit2;
+let units = [];
+let example;
+function preload(){
+  Battlemap2 = loadImage("Assests/Battlemap2.jpg");
+  example = loadImage("Assests/Cav.png");
   
-// }
-// function setup() {
-//   createCanvas(1350, 900);
+}
+function setup() {
+  createCanvas(1350, 900);
 
-//   for(let i = 0 ; i < 23; i++){
-//     units[i] = new UnitsCav(random(width), random(height));
-//   }
+  for(let i = 0 ; i < 23; i++){
+    units[i] = new UnitsCav(random(width), random(height));
+  }
 
-// }
+}
 
 
-// function draw() {
-//   background(220);
+function draw() {
+  background(220);
 
   
-//   image(Battlemap2,0,0);//where it starts to make the background pic
+  image(Battlemap2,0,0);//where it starts to make the background pic
 
 
 
-//   for(let i = 0 ; i < 23; i++){
-//     units[i].show(example);
-//     units[i].update();
-//   }
+  for(let i = 0 ; i < 23; i++){
+    units[i].show(example);
+    units[i].update();
+  }
 
-// }
+}
 
 
 
-// function mousePressed(){
+function mousePressed(){
 
-//   for(let i = 0 ; i < 23; i++){
-//     units[i].checkMouse();
+  for(let i = 0 ; i < 23; i++){
+    units[i].checkMouse();
    
-//   }
+  }
 
-//   // if(collidePointRect(mouseX,mouseY,unit.pos.x,unit.pos.y,unit.width,unit.height) && check === false){
-//   //   check = true;
+  // if(collidePointRect(mouseX,mouseY,unit.pos.x,unit.pos.y,unit.width,unit.height) && check === false){
+  //   check = true;
    
  
-//   // }
-//   // else if (collidePointRect(mouseX,mouseY,unit.pos.x,unit.pos.y,unit.width,unit.height) && check === true){
-//   //   check = false;
+  // }
+  // else if (collidePointRect(mouseX,mouseY,unit.pos.x,unit.pos.y,unit.width,unit.height) && check === true){
+  //   check = false;
 
-//   //   if(unit.pos.x%45 !== 0){
-//   //     unit.pos.x = round(unit.pos.x/45) * 45; //x part of 45
-//   //   }
-//   //   if(unit.pos.y%45 !== 0){
-//   //     unit.pos.y = round(unit.pos.y/45) * 45; //y part of 45
-//   //   }
-//   // }
+  //   if(unit.pos.x%45 !== 0){
+  //     unit.pos.x = round(unit.pos.x/45) * 45; //x part of 45
+  //   }
+  //   if(unit.pos.y%45 !== 0){
+  //     unit.pos.y = round(unit.pos.y/45) * 45; //y part of 45
+  //   }
+  // }
 
-// }
+}
 
